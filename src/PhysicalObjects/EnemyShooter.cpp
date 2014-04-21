22dcfe72832f60fe66de8c3621e6e5616c43de5a
Nathan Farnum <njfarnum@pcp068551pcs.wireless.calpoly.edu>
2014-04-20 18:06:19 -0700

    Updated enemy shooter a bit

---
+++ src/PhysicalObjects/EnemyShooter.cpp
@@ -0,0 +1,29 @@
+#include "EnemyShooter.h"
+
+void EnemyShooter::attack() {
+   //Call shooting function
+}
+
+void EnemyShooter::move(const float dt) {
+   position.x = sin(dt)*this->movementXRange + position.x;
+   position.y = sin(dt)*this->movementYRange + position.y;
+}
+
+
+
+//Change damage
+//Multiply dt by something for calling move if you
+//want to change movement speed vs tick speed
+void EnemyShooter::tick(const float dt) {   
+      
+      //Update AI
+      /*if (beenShot) {
+         this->loseHealth(1);
+         beenShot = false;
+      }*/
+      move(dt);
+      if (seePlayer) {
+         attack();
+      }
+      
+  }

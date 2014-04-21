b8c4552c7acb0f4fd27cdf7805b97d4066241da5
Aaron Jacobs <aaron@aaronmjacobs.com>
2014-04-20 19:44:09 -0700

    Commented out enemy shooter.

--- src/PhysicalObjects/EnemyShooter.cpp
+++ src/PhysicalObjects/EnemyShooter.cpp
@@ -1,29 +1,29 @@
-#include "EnemyShooter.h"
+//#include "EnemyShooter.h"
 
-void EnemyShooter::attack() {
+//void EnemyShooter::attack() {
    //Call shooting function
-}
+//}
 
-void EnemyShooter::move(const float dt) {
-   position.x = sin(dt)*this->movementXRange + position.x;
-   position.y = sin(dt)*this->movementYRange + position.y;
-}
+//void EnemyShooter::move(const float dt) {
+//   position.x = sin(dt)*this->movementXRange + position.x;
+//   position.y = sin(dt)*this->movementYRange + position.y;
+//}
 
 
 
 //Change damage
 //Multiply dt by something for calling move if you
 //want to change movement speed vs tick speed
-void EnemyShooter::tick(const float dt) {   
+//void EnemyShooter::tick(const float dt) {   
       
       //Update AI
       /*if (beenShot) {
          this->loseHealth(1);
          beenShot = false;
       }*/
-      move(dt);
-      if (seePlayer) {
-         attack();
-      }
+//      move(dt);
+//      if (seePlayer) {
+//         attack();
+//      }
       
-  }
+//  }

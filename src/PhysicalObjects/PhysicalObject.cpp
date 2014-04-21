8f2aff4149be042e7bfd874c07497de7aec22f06
Aaron Jacobs <aaron@aaronmjacobs.com>
2014-04-21 16:37:44 -0700

    Added give on horizontal collisions to prevent objects from getting caught on things.

--- src/PhysicalObjects/PhysicalObject.cpp
+++ src/PhysicalObjects/PhysicalObject.cpp
@@ -26,8 +26,10 @@ bool PhysicalObject::checkCollision(PhysicalObject *physObj) {
    AxisAlignedBoundingBox tBounds = getBounds();
    AxisAlignedBoundingBox oBounds = physObj->getBounds();
 
+   float give = 0.01f;
+
    //check if objects are on screen via clipping and culling
-   if (tBounds.xMax < oBounds.xMin || tBounds.xMin > oBounds.xMax)
+   if (tBounds.xMax - give < oBounds.xMin || tBounds.xMin + give > oBounds.xMax)
       return false;
    if (tBounds.yMax < oBounds.yMin || tBounds.yMin > oBounds.yMax)
       return false;

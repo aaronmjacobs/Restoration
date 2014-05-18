7d40c19cb6862a620349e47079da291388bebb29
Aaron Jacobs <aaron@aaronmjacobs.com>
2014-05-18 12:34:26 -0700

    Updated serialization for new types.

--- src/Loader.h
+++ src/Loader.h
@@ -10,11 +10,14 @@
 #include <string>
 
 class aiScene;
+class AniMesh;
+class AniModel;
 class Camera;
 class Character;
 class Corona;
 class Enemy;
 class FlatSceneGraph;
+class FollowGeometry;
 class Geometry;
 class Light;
 class Magus;
@@ -42,6 +45,7 @@ struct PhongMaterialData;
 struct SceneObjectData;
 } // namespace
 
+typedef std::map<std::string, SPtr<AniMesh>> AniMeshMap;
 typedef std::map<std::string, SPtr<Mesh>> MeshMap;
 typedef std::map<std::string, SPtr<Shader>> ShaderMap;
 typedef std::map<std::string, SPtr<ShaderProgram>> ShaderProgramMap;
@@ -54,6 +58,7 @@ class Loader {
 protected:
    static SPtr<Loader> instance;
    Assimp::Importer assimpImporter;
+   AniMeshMap aniMeshMap;
    MeshMap meshMap;
    ShaderMap shaderMap;
    ShaderProgramMap shaderProgramMap;
@@ -65,6 +70,7 @@ protected:
    bool isLight(const std::string &className);
    bool isCamera(const std::string &className);
    bool isGeometry(const std::string &className);
+   bool isFollowGeometry(const std::string &className);
    bool isPhysicalObject(const std::string &className);
    bool isScenery(const std::string &className);
    bool isMovableObject(const std::string &className);
@@ -73,6 +79,10 @@ protected:
    bool isEnemy(const std::string &className);
    bool isMagus(const std::string &className);
    bool isCorona(const std::string &className);
+   bool isMesh(const std::string &className);
+   bool isAniMesh(const std::string &className);
+   bool isModel(const std::string &className);
+   bool isAniModel(const std::string &className);
    glm::vec3 loadVec3(const Json::Value &root, bool rgb = false);
    glm::quat loadQuat(const Json::Value &root);
    PhongMaterialData loadPhongMaterialData(SPtr<Scene> scene, const Json::Value &root);
@@ -91,11 +101,14 @@ public:
    GLuint loadTexture(const std::string &fileName);
    GLuint loadCubemap(const std::string &fileName);
 
+   SPtr<AniMesh> loadAniMesh(const Json::Value &root);
+   SPtr<AniModel> loadAniModel(SPtr<Scene> scene, const Json::Value &root);
    SPtr<Camera> loadCamera(SPtr<Scene> scene, const Json::Value &root);
    SPtr<Character> loadCharacter(SPtr<Scene> scene, const Json::Value &root);
    SPtr<Corona> loadCorona(SPtr<Scene> scene, const Json::Value &root);
    SPtr<Enemy> loadEnemy(SPtr<Scene> scene, const Json::Value &root);
-   SPtr<FlatSceneGraph> loadFlatSceneGraph(SPtr<Scene> scene, const Json::Value &root);
+   void loadFlatSceneGraph(SPtr<Scene> scene, const Json::Value &root);
+   SPtr<FollowGeometry> loadFollowGeometry(SPtr<Scene> scene, const Json::Value &root);
    SPtr<Geometry> loadGeometry(SPtr<Scene> scene, const Json::Value &root);
    SPtr<Light> loadLight(SPtr<Scene> scene, const Json::Value &root);
    SPtr<Magus> loadMagus(SPtr<Scene> scene, const Json::Value &root);
@@ -107,7 +120,7 @@ public:
    SPtr<PhysicalObject> loadPhysicalObject(SPtr<Scene> scene, const Json::Value &root);
    SPtr<Player> loadPlayer(SPtr<Scene> scene, const Json::Value &root);
    SPtr<Scene> loadScene(const std::string &fileName);
-   SPtr<SceneGraph> loadSceneGraph(SPtr<Scene> scene, const Json::Value &root);
+   void loadSceneGraph(SPtr<Scene> scene, const Json::Value &root);
    SPtr<SceneObject> loadSceneObject(SPtr<Scene> scene, const Json::Value &root);
    SPtr<Scenery> loadScenery(SPtr<Scene> scene, const Json::Value &root);
    SPtr<Shader> loadShader(const Json::Value &root);

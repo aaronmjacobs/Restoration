8b55b02cdca39793c69295c0e082aabaaf1ebc81
Aaron Jacobs <aaron@aaronmjacobs.com>
2014-05-23 14:31:44 -0700

    Added example code.

--- src/Particle.cpp
+++ src/Particle.cpp
@@ -3,12 +3,21 @@
 #include "Model.h"
 #include "Particle.h"
 
+/*
+
+SPtr<Particle> particle = std::make_shared<Particle>(scene);
+particle->setPosition(glm::vec3(18.0f, 15.0f, 0.0f));
+particle->scaleBy(glm::vec3(5.0f));
+scene->getSceneGraph()->add(particle);
+
+*/
+
 SPtr<Model> Particle::particleModel;
 
 void Particle::initialize(SPtr<Scene> scene) {
    Loader &loader = Loader::getInstance();
    SPtr<Mesh> mesh = std::make_shared<Mesh>("data/meshes/particle.obj");
-   SPtr<Material> material = loader.loadMaterial(scene, "otherMaterial");
+   SPtr<Material> material = loader.loadMaterial(scene, "particle");
    particleModel = std::make_shared<Model>(material, mesh);
 }
 
@@ -16,6 +25,7 @@ const std::string Particle::CLASS_NAME = "Particle";
 
 Particle::Particle(SPtr<Scene> scene, const std::string &name)
 :  Geometry(scene, particleModel, name) {
+   acceleration = glm::vec3(0.0f, -9.8f, 0.0f);
 }
 
 Particle::~Particle() {

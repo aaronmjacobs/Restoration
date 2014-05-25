297e8874dead5ba1cb16957a2c9b1bb2417850ad
agragg <agragg@calpoly.edu>
2014-05-25 11:03:50 -0700

    Added createEffect() for a particle effect.

--- src/Particle.h
+++ src/Particle.h
@@ -8,11 +8,15 @@ protected:
    static SPtr<Model> particleModel;
 
    glm::vec3 velocity, acceleration;
+   double endTime;
 
 public:
    static const std::string CLASS_NAME;
 
    static void initialize(SPtr<Scene> scene);
+
+   /* Creates numParts particles with position, velocity, duration(seconds), spread(random component in all directions), gravity (or not), and size. */
+   static void createEffect(SPtr<Scene> scene, glm::vec3 position, glm::vec3 velocity, bool gravityOn, float size, int numParts, double duration, float spread);
 
    Particle(SPtr<Scene> scene, const std::string &name = "");
    virtual ~Particle();

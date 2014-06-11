c11ad682186bb55617d0863c9814089a540fe246
tssanford <tssanfor@calpoly.edu>
2014-06-10 17:25:22 -0700

    fixed ao blur radius

--- data/shaders/fbo_ao_frag.glsl
+++ data/shaders/fbo_ao_frag.glsl
@@ -7,7 +7,7 @@ uniform int uViewportHeight;
 varying vec2 vTexCoord;
 
 const float BLUR = 2.0;
-const int SPAN = 8;
+const int SPAN = 5;
 const float SATURATION = 1.0;
 const float EXPOSURE = 2.3;
 const float THRESHOLD = 0.0065;
